9ff958b49777753dfa67dd1655cae463b8a11453
hsh778205 <heshaohong2015@outlook.com>
2024-03-03 22:25:44 +0800

    cf 1933 Codeforces Round 929 (Div. 3)

---
+++ CodeForces/1933/D.cpp
@@ -0,0 +1,161 @@
+/**************************************************************
+ * Problem: 
+ * Author: Vanilla_chan
+ * Date: 
+ * E-Mail: heshaohong2015@outlook.com
+ **************************************************************/
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<limits.h>
+#define IL inline
+#define re register
+#define LL long long
+#define ULL unsigned long long
+#ifdef TH
+#define debug printf("Now is %d\n",__LINE__);
+#else
+#define debug
+#endif
+#ifdef ONLINE_JUDGE
+char buf[1<<23],* p1=buf,* p2=buf,obuf[1<<23],* O=obuf;
+#define getchar() (p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
+#endif
+using namespace std;
+
+namespace oi
+{
+	inline bool isdigit(const char& ch)
+	{
+		return ch<='9'&&ch>='0';
+	}
+	inline bool isalnum(const char& ch)
+	{
+		return (ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')||(ch>='0'&&ch<='9');
+	}
+	struct istream
+	{
+		char ch;
+		bool fu;
+		template<class T>inline istream& operator>>(T& d)
+		{
+			fu=d=0;
+			while(!isdigit(ch)&&ch!='-') ch=getchar();
+			if(ch=='-') fu=1,ch=getchar();
+			d=ch-'0';ch=getchar();
+			while(isdigit(ch))
+				d=(d<<3)+(d<<1)+(ch^'0'),ch=getchar();
+			if(fu) d=-d;
+			return *this;
+		}
+		inline istream& operator>>(char& ch)
+		{
+			ch=getchar();
+			for(;!isalnum(ch);ch=getchar());
+			return *this;
+		}
+		inline istream& operator>>(string& str)
+		{
+			str.clear();
+			for(;!isalnum(ch);ch=getchar());
+			while(isalnum(ch))
+				str+=ch,ch=getchar();
+			return *this;
+		}
+	}cin;
+	inline int read()
+	{
+		int x=0,fu=0;char ch=getchar();
+		while(!isdigit(ch)&&ch!='-') ch=getchar();
+		if(ch=='-') fu=1,ch=getchar();
+		while(isdigit(ch)) { x=x*10+ch-'0';ch=getchar(); }
+		if(fu) x=-x;
+		return x;
+	}
+	int G[55];
+	template<class T>inline void write(T x)
+	{
+		int g=0;
+		if(x<0) x=-x,putchar('-');
+		do { G[++g]=x%10;x/=10; } while(x);
+		for(int i=g;i>=1;--i) putchar('0'+G[i]);
+		putchar('\n');
+	}
+};
+using oi::read;
+
+#define N 100010
+
+
+int t;
+int n;
+int a[N];
+int cnt,minn;
+int main()
+{
+	cin>>t;
+	while(t--){
+		cin>>n;
+		cnt=0;
+		minn=1000000010;
+		for(int i=1;i<=n;i++)
+		{
+			cin>>a[i];
+			if(minn>a[i]) minn=a[i],cnt=0;
+			if(minn==a[i]) cnt++;
+		}
+		
+		if(cnt>=2)
+		{
+			bool flag=0;
+			for(int i=1;i<=n;i++)
+			{
+				if(a[i]%minn) flag=1;
+			}
+			if(flag) cout<<"YES"<<endl;
+			else cout<<"NO"<<endl;
+		}
+		
+		else cout<<"YES"<<endl;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+/*
+same number put same
+
+same number not put head
+
+mod will make number smaller and smaller
+
+greater line is safe
+
+4%3=1
+
+cnt1 <=1(put first)
+
+2 3 4 5 
+
+3 2 2 
+
+
+
+
+
+
+*/
---
+++ CodeForces/1933/E.cpp
@@ -0,0 +1,140 @@
+/**************************************************************
+ * Problem: 
+ * Author: Vanilla_chan
+ * Date: 
+ * E-Mail: heshaohong2015@outlook.com
+ **************************************************************/
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<limits.h>
+#define IL inline
+#define re register
+#define LL long long
+#define ULL unsigned long long
+#ifdef TH
+#define debug printf("Now is %d\n",__LINE__);
+#else
+#define debug
+#endif
+#ifdef ONLINE_JUDGE
+char buf[1<<23],* p1=buf,* p2=buf,obuf[1<<23],* O=obuf;
+#define getchar() (p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
+#endif
+using namespace std;
+
+namespace oi
+{
+	inline bool isdigit(const char& ch)
+	{
+		return ch<='9'&&ch>='0';
+	}
+	inline bool isalnum(const char& ch)
+	{
+		return (ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')||(ch>='0'&&ch<='9');
+	}
+	struct istream
+	{
+		char ch;
+		bool fu;
+		template<class T>inline istream& operator>>(T& d)
+		{
+			fu=d=0;
+			while(!isdigit(ch)&&ch!='-') ch=getchar();
+			if(ch=='-') fu=1,ch=getchar();
+			d=ch-'0';ch=getchar();
+			while(isdigit(ch))
+				d=(d<<3)+(d<<1)+(ch^'0'),ch=getchar();
+			if(fu) d=-d;
+			return *this;
+		}
+		inline istream& operator>>(char& ch)
+		{
+			ch=getchar();
+			for(;!isalnum(ch);ch=getchar());
+			return *this;
+		}
+		inline istream& operator>>(string& str)
+		{
+			str.clear();
+			for(;!isalnum(ch);ch=getchar());
+			while(isalnum(ch))
+				str+=ch,ch=getchar();
+			return *this;
+		}
+	}cin;
+	inline int read()
+	{
+		int x=0,fu=0;char ch=getchar();
+		while(!isdigit(ch)&&ch!='-') ch=getchar();
+		if(ch=='-') fu=1,ch=getchar();
+		while(isdigit(ch)) { x=x*10+ch-'0';ch=getchar(); }
+		if(fu) x=-x;
+		return x;
+	}
+	int G[55];
+	template<class T>inline void write(T x)
+	{
+		int g=0;
+		if(x<0) x=-x,putchar('-');
+		do { G[++g]=x%10;x/=10; } while(x);
+		for(int i=g;i>=1;--i) putchar('0'+G[i]);
+		putchar('\n');
+	}
+};
+using oi::read;
+
+#define N 100010
+
+
+int t;
+int n;
+int a[N];
+int sum[N];
+int q;
+int main()
+{
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		for(int i=1;i<=n;i++)
+		{
+			cin>>a[i];
+			sum[i]=sum[i-1]+a[i];
+		}
+		
+		
+		cin>>q;
+		while(q--)
+		{
+			int l,u,r2,r1;
+			cin>>l>>u;
+			//find r,make sum[r]-sum[l-1]<=u
+			//so, sum[r]-sum[l-1]<u
+			//sum[r]<u+sum[l-1]
+			r2=lower_bound(sum+l, sum+n+1, u+0.5+sum[l-1])-sum;
+			if(r2==n+1) r2--;
+			r1=r2-1;
+			if(r1<=l) r1=l;
+			//cout<<"r1="<<r1<<" s-s="<<sum[r1]-sum[l-1]<<" r2="<<r2<<" s-s="<<sum[r2]-sum[l-1]<<endl;
+			if(u+0.5+sum[l-1]-sum[r1]<=sum[r2]-sum[l-1]-u-0.5) cout<<r1<<" ";			
+			else cout<<r2<<" ";
+		}
+		cout<<endl;
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
