9736cacbdd867b173b18eec5189539a60e836828
Vanilla_chan <1751431516@qq.com>
2025-02-19 20:02:05 +0800

    abc389

---
+++ Atcoder/abc389/C.cpp
@@ -0,0 +1,80 @@
+/**************************************************************
+ * Problem: 
+ * Author: ArisuVanilla
+ * Date: 
+ * E-Mail: heshaohong2015@outlook.com
+ **************************************************************/
+#include<bits/stdc++.h>
+#define IL inline
+#define re register
+#define LL long long
+#define ll long long
+#define ULL unsigned long long
+#define ull unsigned long long
+#define mk(a,b) make_pair(a,b)
+#define pb(a,b) push_back(a,b)
+#define endl '\n'
+#ifdef TH
+#define debug cout<<"Now is "<<__LINE__<<endl;
+#else
+#define debug
+#endif
+
+std::string COLORS[]={"\033[0m","\033[91m","\033[92m","\033[93m","\033[94m","\033[95m","\033[96m"};
+std::string COLOR_NAMES[]={"RESET","RED","GREEN","YELLOW","BLUE","MAGENTA","CYAN"};
+
+#define DEBUG(x,color)                   \
+cout<<COLORS[color];                     \
+cout << #x << " : " << x << endl;        \
+cout<<COLORS[0];                         \
+cout.flush();
+
+#define LOOP(While_times) while(While_times-->0)
+using namespace std;
+
+
+#define N 1000010
+int q;
+LL que[N];
+LL sum[N];
+int l,r;
+void solve()
+{
+	cin>>q;
+	l=r=1;
+	while(q--)
+	{
+		int op,x;
+		cin>>op;
+		if(op!=2) cin>>x;
+		if(op==1)
+		{
+			que[r]=x;
+			sum[r]=sum[r-1]+que[r];
+			r++;
+		}
+		else if(op==2)
+		{
+			l++;
+		}
+		else if(op==3)
+		{
+			cout<<sum[l+x-2]-sum[l-1]<<endl;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout.precision(10);
+	int t=1;
+//	cin>>t;
+	LOOP(t)
+	{
+		solve();
+	}
+	return 0;
+}
---
+++ Atcoder/abc389/D.cpp
@@ -0,0 +1,72 @@
+/**************************************************************
+ * Problem: 
+ * Author: ArisuVanilla
+ * Date: 
+ * E-Mail: heshaohong2015@outlook.com
+ **************************************************************/
+#include<bits/stdc++.h>
+#define IL inline
+#define re register
+#define LL long long
+#define ll long long
+#define ULL unsigned long long
+#define ull unsigned long long
+#define mk(a,b) make_pair(a,b)
+#define pb(a,b) push_back(a,b)
+#define endl '\n'
+#ifdef TH
+#define debug cout<<"Now is "<<__LINE__<<endl;
+#else
+#define debug
+#endif
+
+std::string COLORS[]={"\033[0m","\033[91m","\033[92m","\033[93m","\033[94m","\033[95m","\033[96m"};
+std::string COLOR_NAMES[]={"RESET","RED","GREEN","YELLOW","BLUE","MAGENTA","CYAN"};
+
+#define DEBUG(x,color)                   \
+cout<<COLORS[color];                     \
+cout << #x << " : " << x << endl;        \
+cout<<COLORS[0];                         \
+cout.flush();
+
+#define LOOP(While_times) while(While_times-->0)
+using namespace std;
+
+
+#define N 1000010
+double R;
+double eps=1e-6;
+void solve()
+{
+	cin>>R;
+	LL ans=0;
+	for(int i=0;i<=R;i++)
+	{
+		//calc up line
+		double y=i+0.5;
+		double len2=R*R-y*y;
+		if(len2<-eps) continue;
+		double len=sqrt(len2);
+		LL now=0;
+		if(len>=0.5-eps) now++,len-=0.5;
+		now+=2*(LL)(len);
+		if(i==0) ans+=now;
+		else ans+=2*now;
+	}
+	cout<<ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout.precision(10);
+	int t=1;
+//	cin>>t;
+	LOOP(t)
+	{
+		solve();
+	}
+	return 0;
+}
