b41367a00bb9e1151927177135a2b9bcfa9dc3c2
Vanilla_chan <heshaohong2015@outlook.com>
2025-06-27 00:59:39 +0800

    【每日一题】

--- CodeForces/2063/C.cpp
+++ CodeForces/2063/C.cpp
@@ -35,10 +35,61 @@ using namespace std;
 
 #define N 1000010
 
-vector<int>edge[]
+vector<int>edge[N];
+set<int>near[N];
+int d[N];
+set<pair<int,int>>s;
+int n;
 void solve()
 {
-	
+	cin>>n;
+	//clear
+	s.clear();
+	for(int i=1;i<=n;i++)
+	{
+		edge[i].clear();
+		near[i].clear();
+		d[i]=0;
+	}
+	//read
+	for(int i=1;i<n;i++)
+	{
+		int x,y;
+		cin>>x>>y;
+		edge[x].push_back(y);
+		edge[y].push_back(x);
+		near[x].insert(y);
+		near[y].insert(x);
+		d[x]++;
+		d[y]++;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		s.insert(make_pair(-d[i],i));
+	}
+	int ans=0;
+	//calc
+	for(int i=1;i<=n;i++)
+	{
+//		set<pair<int,int>>s2=s;
+		for(unsigned int j=0;j<edge[i].size();j++)
+		{
+			s.erase(make_pair(-d[edge[i][j]],edge[i][j]));
+			s.insert(make_pair(-d[edge[i][j]]+1,edge[i][j]));
+//			DEBUG(s2.size(),2);
+		}
+		s.erase(make_pair(-d[i],i));
+//		DEBUG(s2.size(),3);
+		ans=max(ans,d[i]-s.begin()->first-1);
+		for(unsigned int j=0;j<edge[i].size();j++)
+		{
+			s.insert(make_pair(-d[edge[i][j]],edge[i][j]));
+			s.erase(make_pair(-d[edge[i][j]]+1,edge[i][j]));
+//			DEBUG(s2.size(),2);
+		}
+		s.insert(make_pair(-d[i],i));
+	}
+	cout<<ans<<endl;
 }
 
 int main()
@@ -48,7 +99,7 @@ int main()
 	cout.tie(0);
 	cout.precision(10);
 	int t=1;
-//	cin>>t;
+	cin>>t;
 	LOOP(t)
 	{
 		solve();
