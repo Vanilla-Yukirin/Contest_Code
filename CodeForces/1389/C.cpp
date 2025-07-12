0c716fff2a2023fca9fa7d33cd8372ac02e39c2e
Vanilla_chan <1751431516@qq.com>
2025-07-12 22:00:54 +0800

    团队赛做题

---
+++ CodeForces/1389/C.cpp
@@ -0,0 +1,89 @@
+/**************************************************************
+ * Problem: 
+ * Author: ArisuVanilla
+ * Date: 
+ * E-Mail: heshaohong2015@outlook.com
+ **************************************************************/
+#include<bits/stdc++.h>
+#define IL inline
+#define re register
+#define LL long long
+#define ll long long
+#define ULL unsigned long long
+#define ull unsigned long long
+#define mk(a,b) make_pair(a,b)
+#define pb(a,b) push_back(a,b)
+#define endl '\n'
+#ifdef TH
+#define debug cout<<"Now is "<<__LINE__<<endl;
+#else
+#define debug
+#endif
+
+std::string COLORS[]={"\033[0m","\033[91m","\033[92m","\033[93m","\033[94m","\033[95m","\033[96m"};
+std::string COLOR_NAMES[]={"RESET","RED","GREEN","YELLOW","BLUE","MAGENTA","CYAN"};
+
+#define DEBUG(x,color)                   \
+cout<<COLORS[color];                     \
+cout << #x << " : " << x << endl;        \
+cout<<COLORS[0];                         \
+cout.flush();
+
+#define LOOP(While_times) while(While_times-->0)
+using namespace std;
+
+
+#define N 1000010
+
+
+
+int n;
+int a[N];
+void solve()
+{
+	string str;cin>>str;
+	n=str.size();
+	for(int i=1;i<=n;i++)
+	{
+		a[i]=str[i-1]-'0';
+	}
+	int ans=n+1;
+	for(int i=0;i<=9;i++)
+	{
+		for(int j=0;j<=9;j++)
+		{
+			int del=0,cnt=0;
+			for(int k=1;k<=n;k++)
+			{
+				if(cnt&1)
+				{
+					if(a[k]!=i) del++;
+					else cnt++;
+				}
+				else
+				{
+					if(a[k]!=j) del++;
+					else cnt++;
+				}
+			}
+			if((cnt&1)&&i!=j) del++;
+			ans=min(ans,del);
+		}
+	}
+	cout<<ans<<endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout.precision(10);
+	int t=1;
+	cin>>t;
+	LOOP(t)
+	{
+		solve();
+	}
+	return 0;
+}
