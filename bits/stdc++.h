0f0e440d414cf0e00c0460fa33e23de7bc3d3bc5
Yuki <Vanilla_Yukirin@outlook.com>
2026-03-19 20:33:31 +0800

    添加万能头文件和自定义TH

---
+++ bits/stdc++.h
@@ -0,0 +1,39 @@
+// GCC style bits/stdc++.h header for competitive programming
+#ifndef _BITS_STDC_PLUS_PLUS_H_
+#define _BITS_STDC_PLUS_PLUS_H_
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <ctype.h>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cstdint>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <bitset>
+#include <numeric>
+#include <memory>
+#include <functional>
+#include <utility>
+#include <iterator>
+#include <array>
+
+#endif // _BITS_STDC_PLUS_PLUS_H_
