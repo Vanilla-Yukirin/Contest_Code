8b5505b3f621e8e743ed9148beb5f68917edbaf3
Yuki <Vanilla_Yukirin@outlook.com>
2026-03-20 00:43:35 +0800

    重构 stdc++.h 头文件，更新版权信息并添加更多 C++ 标准库包含

--- common/bits/stdc++.h
+++ common/bits/stdc++.h
@@ -1,39 +1,116 @@
-// GCC style bits/stdc++.h header for competitive programming
-#ifndef _BITS_STDC_PLUS_PLUS_H_
-#define _BITS_STDC_PLUS_PLUS_H_
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
+// C++ includes used for precompiling -*- C++ -*-
+ 
+// Copyright (C) 2003-2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+ 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+ 
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+ 
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+ 
+/** @file stdc++.h
+ *  This is an implementation file for a precompiled header.
+ */
+ 
+// 17.4.1.2 Headers
+ 
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cmath>
 #include <ctime>
-#include <ctype.h>
-#include <cassert>
-#include <cctype>
-#include <climits>
+ 
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
 #include <cstdint>
-
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+ 
+// C++
 #include <algorithm>
-#include <string>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <list>
 #include <bitset>
-#include <numeric>
-#include <memory>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
 #include <functional>
-#include <utility>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
 #include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+#if __cplusplus >= 201103L
 #include <array>
-
-#endif // _BITS_STDC_PLUS_PLUS_H_
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
